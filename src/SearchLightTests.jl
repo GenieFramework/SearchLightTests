b14f97d52733e85c8bd45fb8d2b50ab43c7b2c0b
Adrian Salceanu <adrian.salceanu@gmail.com>
2022-08-16 09:25:55 +0200

    Files generated by PkgTemplates
    PkgTemplates version: 0.7.29

---
+++ src/SearchLightTests.jl
@@ -0,0 +1,5 @@
+module SearchLightTests
+
+# Write your package code here.
+
+end
---
+++ test/runtests.jl
@@ -0,0 +1,6 @@
+using SearchLightTests
+using Test
+
+@testset "SearchLightTests.jl" begin
+    # Write your tests here.
+end
